7f81ce5112680272e5f55b0762f5049e28241b25
wideputin117 <takermanish7@gmail.com>
2023-12-31 23:26:02 +0530

    constants pushed

---
+++ constants/data.js
@@ -0,0 +1,62 @@
+ export const data = [
+{   
+
+    "title":"Weather Application",
+    "img": "weather.svg",
+},
+{   
+
+    "title":"Text Translation",
+    "img": "Translation-icon.svg",
+},
+{   
+
+    "title":"Blogg App",
+    "img": "blog.svg",
+},
+{   
+
+    "title":"Random Quote Generator",
+    "img": "quote.svg",
+},
+{   
+
+    "title":"Chat App",
+    "img": "chat.svg",
+},
+{   
+
+    "title":"To do App",
+    "img": "to-do-list.svg",
+},
+{   
+
+    "title":"Chat Gpt App",
+    "img": "business-chat.svg",
+},
+{   
+
+    "title":"NEXT.JS API CRUD APP",
+    "img": "next.svg",
+},
+/*{   
+
+    "title":"",
+    "img": "",
+},
+{   
+
+    "title":"",
+    "img": "",
+},
+{   
+
+    "title":"",
+    "img": "",
+},
+{   
+
+    "title":"",
+    "img": "",
+}, */
+]
